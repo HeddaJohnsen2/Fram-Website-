3d3db4858efdc0926fec397abd0936ea7565060b
Hedda Johnsen <hedda.johnsen@hotmail.no>
2024-12-10 15:06:45 +0000

    fixed the menu for both @medias
    fixed both the text, layout and color of the menus for both mobile and desktop. i also added a correct icon for the closing button.

--- script.js
+++ script.js
@@ -3,12 +3,10 @@ document.addEventListener("DOMContentLoaded", () => {
   const openMenu = document.getElementById("openHamburgerMenu");
   const closeMenu = document.getElementById("closedMenuButton");
 
-  // Open the menu
   openMenu.addEventListener("click", () => {
     menu.classList.add("show");
   });
 
-  // Close the menu
   closeMenu.addEventListener("click", () => {
     menu.classList.remove("show");
   });
