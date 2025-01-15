6e5652784c8fc3f0cc4565c16ce0fc9fb849559c
Hedda Johnsen <hedda.johnsen@hotmail.no>
2025-01-15 21:05:13 +0100

    add source
    added a source the correct way

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 // to make it easier to start up the website i wanted to use a server with express.js
-//source: https://javascript.plainenglish.io/deploying-a-localhost-server-with-node-js-and-express-js-58775f098407
-//https://expressjs.com/en/starter/static-files.html
+//source:
+//Brice, M (June, 10 2020). Deploying A Localhost Server With Node.js and Express.js. Medium. Retrived january 2025.
+//From: https://javascript.plainenglish.io/deploying-a-localhost-server-with-node-js-and-express-js-58775f098407
+
+//Open Js Foundation (n.d) Serving static files in Express. Express.js:
+//retrived january 2025. From: https://expressjs.com/en/starter/static-files.html
 
 const express = require("express");
 const path = require("path");
